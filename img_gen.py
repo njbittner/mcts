02f3f7bd6116559af8005c08e3c13da536b69281
Nathan Bittner <njbittner@gmail.com>
2020-03-05 21:51:46 -0800

    trying to get streaming images to browser working

--- img_gen.py
+++ img_gen.py
@@ -2,6 +2,9 @@
 import time
 import math
 import cairo
+import flask
+import numpy as np
+import io
 
 WIDTH, HEIGHT = 256, 256
 
@@ -10,23 +13,40 @@ ctx = cairo.Context(surface)
 
 ctx.scale(WIDTH, HEIGHT)  # Normalizing the canvas
 
+memory_buffer = io.BytesIO()
 
-i = 0
-while True:
-    pat = cairo.LinearGradient(0.0, 0.0, 0.0, 1.0)
-    pat.add_color_stop_rgba(1, 0.7, 0, 0, 0.5)  # First stop, 50% opacity
-    pat.add_color_stop_rgba(0, 0.9, 0.7, 0.2, 1)  # Last stop, 100% opacity
-    ctx.rectangle(0, 0, 1, 1)  # Rectangle(x0, y0, x1, y1)
-    ctx.set_source(pat)
-    ctx.fill()
+app = flask.Flask(__name__)
 
-    # Arc(cx, cy, radius, start_angle, stop_angle)
-    ctx.arc(0.5, 0.5, .3, math.pi*i/2 - 5,  math.pi*i/2)
+@app.route('/')
+def index():
+    return flask.render_template('index3.html')
 
-    ctx.set_source_rgb(0.3, 0.2, 0.5)  # Solid color
-    ctx.set_line_width(0.02)
-    ctx.stroke()
+@app.route("/next_img")
+def next_img():
+    return flask.Response(img_generator(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-    surface.write_to_png("example.png")  # Output to PNG
-    time.sleep(0.5)
-    i +=1
+def img_generator():
+    i = -1
+    while True:
+        i +=1
+        pat = cairo.LinearGradient(0.0, 0.0, 0.0, 1.0)
+        pat.add_color_stop_rgba(1, 0.7, 0, 0, 0.5)  # First stop, 50% opacity
+        pat.add_color_stop_rgba(0, 0.9, 0.7, 0.2, 1)  # Last stop, 100% opacity
+        ctx.rectangle(0, 0, 1, 1)  # Rectangle(x0, y0, x1, y1)
+        ctx.set_source(pat)
+        ctx.fill()
+
+        # Arc(cx, cy, radius, start_angle, stop_angle)
+        ctx.arc(0.5, 0.5, .3, math.pi*i/2 - 5,  math.pi*i/2)
+
+        ctx.set_source_rgb(0.3, 0.2, 0.5)  # Solid color
+        ctx.set_line_width(0.02)
+        ctx.stroke()
+        memory_buffer.seek(0)
+        surface.write_to_png(memory_buffer)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/png\r\n\r\n' + memory_buffer.read() + b'\r\n')
+
+
+if __name__ == "__main__":
+    app.run(port=8212, debug=True)
