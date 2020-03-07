b4faef45b16d448840fcef795c6782b701a2e7b0
Nathan Bittner <njbittner@gmail.com>
2020-03-06 22:09:30 -0800

    stream

--- img_gen.py
+++ img_gen.py
@@ -17,35 +17,47 @@ memory_buffer = io.BytesIO()
 
 app = flask.Flask(__name__)
 
+
+class Camera(object):
+    def __init__(self):
+        self.frames = [open(f + '.png', 'rb').read() for f in ['1', '2', '3']]
+
+    def get_frame(self):
+        return self.frames[int(time()) % 3]
+
+
 @app.route('/')
 def index():
     return flask.render_template('index3.html')
 
+
 @app.route("/next_img")
 def next_img():
-    return flask.Response(img_generator(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return flask.Response(img_generator(Camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-def img_generator():
-    i = -1
+
+def img_generator(camera):
+    # i = -1
     while True:
-        i +=1
-        pat = cairo.LinearGradient(0.0, 0.0, 0.0, 1.0)
-        pat.add_color_stop_rgba(1, 0.7, 0, 0, 0.5)  # First stop, 50% opacity
-        pat.add_color_stop_rgba(0, 0.9, 0.7, 0.2, 1)  # Last stop, 100% opacity
-        ctx.rectangle(0, 0, 1, 1)  # Rectangle(x0, y0, x1, y1)
-        ctx.set_source(pat)
-        ctx.fill()
+        frame = camera.get_frame()
+        # i +=1
+        # pat = cairo.LinearGradient(0.0, 0.0, 0.0, 1.0)
+        # pat.add_color_stop_rgba(1, 0.7, 0, 0, 0.5)  # First stop, 50% opacity
+        # pat.add_color_stop_rgba(0, 0.9, 0.7, 0.2, 1)  # Last stop, 100% opacity
+        # ctx.rectangle(0, 0, 1, 1)  # Rectangle(x0, y0, x1, y1)
+        # ctx.set_source(pat)
+        # ctx.fill()
 
-        # Arc(cx, cy, radius, start_angle, stop_angle)
-        ctx.arc(0.5, 0.5, .3, math.pi*i/2 - 5,  math.pi*i/2)
+        # # Arc(cx, cy, radius, start_angle, stop_angle)
+        # ctx.arc(0.5, 0.5, .3, math.pi*i/2 - 5,  math.pi*i/2)
 
-        ctx.set_source_rgb(0.3, 0.2, 0.5)  # Solid color
-        ctx.set_line_width(0.02)
-        ctx.stroke()
-        memory_buffer.seek(0)
-        surface.write_to_png(memory_buffer)
+        # ctx.set_source_rgb(0.3, 0.2, 0.5)  # Solid color
+        # ctx.set_line_width(0.02)
+        # ctx.stroke()
+        # memory_buffer.seek(0)
+        # surface.write_to_png(memory_buffer)
         yield (b'--frame\r\n'
-               b'Content-Type: image/png\r\n\r\n' + memory_buffer.read() + b'\r\n')
+               b'Content-Type: image/png\r\n\r\n' + frame + b'\r\n')
 
 
 if __name__ == "__main__":
